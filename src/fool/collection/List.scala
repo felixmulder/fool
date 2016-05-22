5d06597b99fc26d40d8d7ae0e01fcd3a40100977
Felix Mulder <felix.mulder@gmail.com>
2016-05-22 13:11:36 +0200

    Remove `Seq` add a couple of missing methods to `List`

--- src/fool/collection/List.scala
+++ src/fool/collection/List.scala
@@ -19,8 +19,36 @@ package collection
  * list are evaluated in a truly lazy manner. Only the head will be evaluated
  * on prints.
  */
-sealed trait List[+A] extends Seq[A] { self =>
+sealed trait List[+A] { self =>
   def isEmpty: Boolean
+
+  def nonEmpty: Boolean = !isEmpty
+
+  def ++[B >: A](other: => List[B]): List[B]
+
+  def apply(index: Int): Option[A]
+
+  @inline def applyOrElse[B >: A](index: Int)(default: => B): B =
+    apply(index).getOrElse(default)
+
+  def equalMembers[B >: A](other: List[B]): Boolean
+
+  def collect[B](pf: scala.PartialFunction[A, B]) =
+    foldRight(List.empty[B])((elem, acc) => if (pf.isDefinedAt(elem)) (pf(elem) :: acc) else acc)
+
+  def filter(f: A => Boolean): List[A] =
+    foldRight(List.empty[A]){ (elem, acc) =>
+      if (f(elem)) collection.::(() => elem, () => acc)
+      else acc
+    }
+
+  def map[B](f: A => B): List[B] =
+    foldRight(List.empty[B]) { (hd,tl) => collection.::(() => f(hd), () => tl) }
+
+  def foldRight[B](start: => B)(f: (A, => B) => B): B
+
+  def flatMap[B](f: A => List[B]): List[B] =
+    foldRight(List.empty[B])((elem, acc) => f(elem) ++ acc)
 
   def ::[B >: A](x: => B): List[B] =
     collection.::(() => x, () => this)
@@ -30,10 +58,6 @@ sealed trait List[+A] extends Seq[A] { self =>
       case (ss: List[_], xs: List[_]) => ss equalMembers xs
       case _ => false
     }
-
-  override def map[B](f: A => B): List[B] = Nil
-
-  override def foldRight[B](start: => B)(f: (A, => B) => B): B = start
 }
 
 object List {
@@ -47,28 +71,41 @@ object List {
 /**
  * The `NonEmptyList` implements equality for its subtypes
  */
-sealed trait NonEmptyList[+A] extends List[A] with NonEmptySeq[A] { self =>
-  override def equalMembers[B >: A](other: Seq[B]): Boolean = (self, other) match {
+sealed trait NonEmptyList[+A] extends List[A] { self =>
+  def isEmpty = false
+
+  def head: A
+  def tail: List[A]
+
+  override def apply(index: Int): Option[A] =
+    if (index > 0) (tail: @switch) match {
+      case Nil => None
+      case xs: NonEmptyList[_] => xs.apply(index - 1)
+    }
+    else if (index < 0) None
+    else Some(head)
+
+  override def equalMembers[B >: A](other: List[B]): Boolean = (self, other) match {
     case (xs: NonEmptyList[_], ys: NonEmptyList[_]) =>
       (xs.head == ys.head) && (xs.tail equalMembers ys.tail)
     case _ => false
   }
 }
 
-final case class ::[+A](hd: () => A, tl: () => List[A]) extends NonEmptyList[A] {
+final case class ::[+A] private (val hd: () => A, val tl: () => List[A]) extends NonEmptyList[A] {
   lazy val head = hd()
   lazy val tail = tl()
 
-  override def toString: String = s"List($head, ?)"
+  def ++[B >: A](other: => List[B]): List[B] =
+    collection.::(() => head, () => { tail ++ other })
 
-  override def map[B](f: A => B): List[B] =
-    foldRight(List.empty[B]) { (hd,tl) => collection.::(() => f(hd), () => tl) }
-
-  override def foldRight[B](start: => B)(f: (A, => B) => B): B =
+  def foldRight[B](start: => B)(f: (A, => B) => B): B =
     this match {
       case hd :: tl => f(hd(), tl().foldRight(start)(f))
       case _ => start
     }
+
+  override def toString: String = s"List($head, ?)"
 }
 
 object :: {
@@ -81,7 +118,13 @@ object :: {
 final case object Nil extends List[Nothing] {
   def isEmpty = true
 
-  override def equalMembers[B >: Nothing](other: Seq[B]) = other eq Nil
+  def ++[B >: Nothing](other: => List[B]): List[B] = other
 
-  override def apply(index: Int) = None
+  def foldRight[B](start: => B)(f: (Nothing, => B) => B): B = start
+
+  def equalMembers[B >: Nothing](other: List[B]) = other eq Nil
+
+  def apply(index: Int) = None
+
+  override def toString = "Nil"
 }
--- test/fool/collection/List.scala
+++ test/fool/collection/List.scala
@@ -70,6 +70,15 @@ class ListSpec extends FlatSpec with Matchers {
     (Nil: List[Int]).apply(1).getOrElse(-1) should be (-1)
   }
 
+  it should "be able to handle `applyOrElse`" in {
+    val list = 0 :: 1 :: 2 :: Nil
+
+    list.applyOrElse(0)(-1) should be (0)
+    list.applyOrElse(1)(-1) should be (1)
+    list.applyOrElse(2)(-1) should be (2)
+    list.applyOrElse(3)(-1) should be (-1)
+  }
+
   it should "handle `map` correctly" in {
     val list       = 1 :: 2 :: 3 :: 4 :: 5  :: 6  :: Nil
     val listTimes2 = 2 :: 4 :: 6 :: 8 :: 10 :: 12 :: Nil
@@ -105,5 +114,84 @@ class ListSpec extends FlatSpec with Matchers {
     list.foldRight(0)(_ + _) should be (10)
 
     (Nil: List[Int]).foldRight(0)(_ * _) should be (0)
+  }
+
+  it should "be able to append another list" in {
+    val xs = List(1, 2) ++ Nil
+    val ys = List(1, 2) ++ List(3, 4)
+    val zs = Nil        ++ List(1, 2)
+
+    xs should be (List(1, 2))
+    ys should be (List(1, 2, 3, 4))
+    zs should be (List(1, 2))
+    (Nil ++ Nil) should be (Nil)
+  }
+
+  it should "not evaluate when appending" in {
+    var i = 0
+    def incr = { () => i += 1; i }
+
+    val list: List[Int] =
+      collection.::(incr, () => collection.::(incr, () => Nil))
+
+    i should be (0)                     // No increase when creating
+    val concat = (list ++ List(3, 4))
+    i should be (0)                     // No increase after appending list
+    concat should be (List(1, 2, 3, 4))
+    i should be (2)                     // When list is evaluated, the first
+                                        // part should increase counter to 2
+  }
+
+  it should "be able to `flatMap` properly" in {
+    val xss = List(List(1, 2), List(3, 4))
+    (xss flatMap identity) should be (List(1, 2, 3, 4))
+  }
+
+  it should "not evaluate when using `flatMap`" in {
+    var i = 0
+    def incr = { () => i += 1; i }
+
+    val xs =
+      collection.::(incr, () => collection.::(incr, () => Nil))
+    val ys =
+      collection.::(incr, () => collection.::(incr, () => Nil))
+
+    val listOfLists: List[List[Int]] = collection.::(() => xs, () => collection.::(() => ys, () => Nil))
+    val flat: List[Int] = listOfLists flatMap identity
+
+    i should be (0)
+    flat should be (List(1, 2, 3, 4))
+    i should be (4)
+  }
+
+  it should "be able to filter a list" in {
+    val xs = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    xs.filter(_ > 5) should be (List(6, 7, 8, 9, 10))
+  }
+
+  it should "not evaluate list when filtering" in {
+    var i = 0
+    def incr = { () => i += 1; i }
+    val list: List[Int] =
+      collection.::(
+        incr,
+        () => collection.::(
+          incr,
+            () => collection.::(
+              incr,
+                () => Nil)))
+
+    list.filter(_ > 1).map(_ * 2) should be (List(4, 6))
+    i should be (3)
+  }
+
+  it should "be able to collect all" in {
+    val xs = List(1, 2, 3)
+    xs.collect { case x => x } should be (List(1, 2, 3))
+  }
+
+  it should "not collect elements larger than predicate" in {
+    val xs = List(1, 2, 3, 4, 5)
+    xs.collect { case x if x > 3 => x } should be (List(4, 5))
   }
 }
